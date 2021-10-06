1ee6a607ec6ec1762398dad6cd15795672e5bcf4
Saaransh <saaranshsharma28@gmail.com>
2021-10-05 21:34:52 -0400

    start menu

--- Exercise 1/com/company/Main.java
+++ Exercise 1/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+        int input;
+
+        do {
+            System.out.println("Select an input:" + "\n" + "Enter 1 for Scanner" + "\n" + "Enter 2 for Keyboard" +
+                    "\n" + "Enter 3 to exit");
+
+            input = sc.nextInt();
+
+            if(input == 1){
+                System.out.println("f");
+                break;
+            }else if (input == 2){
+                System.out.println("e");
+                break;
+            }else if (input == 3){
+                break;
+            }else {
+                System.out.println("Invalid entry");
+
+            }
+
+        }while (input!=3);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
